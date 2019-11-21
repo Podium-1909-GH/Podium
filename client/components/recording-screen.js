a5ae8d5e2343b7b8083fee176d75c5f51d26e707
Kiara Anderson <kiara.m.anderson@gmail.com>
2019-11-21 17:57:41 -0500

    feat: post route for speech written

--- client/components/recording-screen.js
+++ client/components/recording-screen.js
@@ -16,32 +16,43 @@ class RecordingScreen extends React.Component {
     this.handleStart = this.handleStart.bind(this)
     this.handleEnd = this.handleEnd.bind(this)
 
+    this.recognition = null
+  }
+
+  componentDidMount() {
     window.SpeechRecognition =
       window.webkitSpeechRecognition || window.SpeechRecognition
     if ('SpeechRecognition' in window) {
       this.recognition = new window.SpeechRecognition()
       this.recognition.continuous = true
       this.recognition.onresult = event => {
+        console.log('inside the onresult function!!!')
         const speechToText = event.results[0][0].transcript
+        console.log(speechToText)
         this.setState({...this.state, transcript: speechToText})
-        console.log(this.state)
+        //console.log(this.state)
+        const length = Math.round(
+          (this.state.endTime - this.state.startTime) / 1000
+        )
+        //dispatch thunk
       }
     } else {
+      //NOTE: this is not yet working
       //speech recognition API NOT supported
       this.setState({...this.state, supported: false})
       alert(
         'This browser does not support speech recognition. Please use Chrome or FireFox'
       )
-      
     }
   }
 
   handleStart() {
     if (this.state.supported) {
-      let toggle = !this.state.isTranscribing
       let now = new Date()
+      let toggle = !this.state.isTranscribing
       this.setState({...this.state, startTime: now, isTranscribing: toggle})
       this.recognition.start() //will ask user for permission to access microphone
+      console.log('started!!!')
     } else {
       alert(
         'This browser does not support speech recognition. Please use Chrome or FireFox'
@@ -53,6 +64,7 @@ class RecordingScreen extends React.Component {
     if (this.state.supported) {
       let now = new Date()
       this.recognition.stop()
+      console.log('stopped!!!')
       let toggle = !this.state.isTranscribing
       this.setState({...this.state, endTime: now, isTranscribing: toggle})
     } else {
@@ -67,8 +79,12 @@ class RecordingScreen extends React.Component {
       <div>
         <h1>Practice</h1>
         <h3>Start a new practice session and see how you're doing</h3>
-        <Button onClick={this.handleStart}>Start</Button>
-        <Button onClick={this.handleEnd}>Stop</Button>
+        <Button onClick={this.handleStart} disabled={this.state.isTranscribing}>
+          Start
+        </Button>
+        <Button onClick={this.handleEnd} disabled={!this.state.isTranscribing}>
+          Stop
+        </Button>
         <p>***Placeholder: progress bar***</p>
       </div>
     )
