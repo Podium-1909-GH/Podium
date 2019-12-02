a53e2b019b5877f8f273fab91276b5cfb98d0617
Kiara Anderson <kiara.m.anderson@gmail.com>
2019-12-02 16:57:49 -0500

    feat: about tiles created

--- client/components/home.js
+++ client/components/home.js
@@ -1,30 +1,38 @@
 import React from 'react'
+import Paper from '@material-ui/core/Paper'
 
 //about Podium page
 const Home = () => {
   return (
-    <div>
-      <div>
+    <div className="about">
+      <div className="about-header">
         <div>
           practice a speech, talk, or presentation and receive immediate
           feedback
         </div>
         <div>image</div>
       </div>
-      <div>
-        tiles
-        <div>
-          <div>icon</div>
-          <div>set goals and track your progress</div>
-        </div>
-        <div>
-          <div>icon</div>
-          <div>learn about your habits and compare to well known speakers</div>
-        </div>
-        <div>
-          <div>icon</div>
-          <div>pace, filler words, and sentiment</div>
-        </div>
+      <div className="about-tiles">
+        <Paper className="about-paper" elevation={4}>
+          <div className="about-tile">
+            <div>icon</div>
+            <div>set goals and track your progress</div>
+          </div>
+        </Paper>
+        <Paper className="about-paper" elevation={4}>
+          <div className="about-tile">
+            <div>icon</div>
+            <div>
+              learn about your habits and compare to well known speakers
+            </div>
+          </div>
+        </Paper>
+        <Paper className="about-paper" elevation={4}>
+          <div className="about-tile">
+            <div>icon</div>
+            <div>pace, filler words, and sentiment</div>
+          </div>
+        </Paper>
       </div>
     </div>
   )
