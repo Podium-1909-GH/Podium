ce173671af754b33ff2fcb7d860a8bccc68279ff
Mallory LeeWong <m.c.leewong@gmail.com>
2019-12-02 10:51:57 -0500

    fix: removed title above dashboard bar chart

--- client/components/user-dashboard.js
+++ client/components/user-dashboard.js
@@ -46,7 +46,6 @@ class UserDashboard extends React.Component {
           </Typography>
         ) : (
           <React.Fragment>
-            <Typography variant="h5">All of your speeches</Typography>
             <DashboardMainWrapper speeches={this.state.speeches} />
             <hr />
             <Typography variant="h5">
