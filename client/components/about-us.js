e92a770ab59fee7cc3c315cb8e545b6d0ed3151d
Kiara Anderson <kiara.m.anderson@gmail.com>
2019-12-02 09:44:09 -0500

    merge: commiting to pull from master

--- client/components/about-us.js
+++ client/components/about-us.js
@@ -35,7 +35,21 @@ const AboutUs = () => {
           <a href="https://github.com/kiaramand">
             <img className="icon" src={github} />
           </a>
-          <p>Description</p>
+          <p>
+            I've spent the last eight years exploring the cross-section of
+            technology, innovation, and creativity. My journey started with an
+            engineering and art/design education in Michigan, then made its way
+            to NYC. Here I learned how to operate on a global scale by building
+            regulatory and efficiency tools for teams spanning from New York to
+            Singapore. Next I shifted my focus towards technical design -
+            working with Solutions Architects, Application Risk Managers, Site
+            Reliability Engineers, and countless others to provide security and
+            satisfaction for millions of consumer banking customers. Now I'm
+            wrapping up an education in Fullstack Software Development. I've
+            adding JavaScript, React/Redux, Node and many other skills to my
+            tool belt with the goal of building a limitless foundation for
+            creation.{' '}
+          </p>
         </div>
       </div>
 
