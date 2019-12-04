a236506409a8722e7cc9788108c61e2b89720ddc
Haley Sambursky <hsambursky46@gmail.com>
2019-12-04 15:29:15 -0500

    style: pie chart recolor

--- client/components/speech-sentiment-d3.js
+++ client/components/speech-sentiment-d3.js
@@ -15,11 +15,11 @@ export default class D3SentimentChart {
 
       // set the color scale
       if (data.name === 'Positive') {
-        data.color = '#2ECC71'
+        data.color = '#11C3D0'
       } else if (data.name === 'Negative') {
-        data.color = '#E67E22'
+        data.color = '#E445A8'
       } else {
-        data.color = '#D6EAF8'
+        data.color = '#4652B1'
       }
     })
 
@@ -36,7 +36,44 @@ export default class D3SentimentChart {
         'transform',
         `translate(${RADIUS + MARGIN.LEFT}, ${RADIUS + MARGIN.TOP})`
       )
+    vis.Tooltip = d3
+      .select(element)
+      .append('div')
+      .style('opacity', 0)
+      .attr('class', 'tooltip')
+      .style('background-color', 'white')
+      .style('color', '#4652B1')
+      .style('border', 'solid')
+      .style('border-color', '#4652B1')
+      .style('border-width', '3px')
+      .style('border-radius', '3px')
+      .style('width', 'fit-content')
+      .style('text-align', 'center')
+      .style('padding', '5px')
+      .style('font-size', '80%')
+      .style('position', 'absolute')
+    let mouseover = function(d) {
+      vis.Tooltip.style('opacity', 1)
+      d3
+        .select(this)
+        .style('stroke', '#4652B1')
+        .style('stroke-width', 3)
+        .style('opacity', 1)
+    }
+    let mousemove = function(d) {
+      vis.Tooltip.html(`${d.data.name} Count: ${d.data.count}`)
+        .style('left', event.pageX + 10 + 'px')
+        .style('top', event.pageY + 'px')
+    }
 
+    let mouseleave = function(d) {
+      vis.Tooltip.style('opacity', 0)
+      d3
+        .select(this)
+        .style('stroke', 'white')
+        .style('stroke-width', 1)
+        .style('opacity', 0.8)
+    }
     //Create pie generator getting startAngle and endAngle
     const pieGenerator = d3
       .pie()
@@ -50,7 +87,7 @@ export default class D3SentimentChart {
     // Create an arc generator with configuration
     const arcGenerator = d3
       .arc()
-      .innerRadius(0)
+      .innerRadius(70)
       .outerRadius(RADIUS)
 
     //connect Data
@@ -65,33 +102,36 @@ export default class D3SentimentChart {
       .attr('d', arcGenerator)
       .attr('fill', d => d.data.color)
       .attr('stroke', 'white')
-      .style('stroke-width', '0.5px')
+      .style('stroke-width', '1')
       .style('opacity', 0.7)
+      .on('mouseover', mouseover)
+      .on('mousemove', mousemove)
+      .on('mouseleave', mouseleave)
 
     //Percentage outside Pie Chart- example from https://bl.ocks.org/farazshuja/e2cb52828c080ba85da5458e2304a61f?fbclid=IwAR1We_VcLDjFBre6Fv60rT3OtHIEVdmTttkh7KG0ZHRrfzLBIcHIgV1QUYQ
-    vis.svg
-      .selectAll('.arc')
-      .data(arcData)
-      .enter()
-      .append('text')
-      .attr('transform', function(d) {
-        var _d = arcGenerator.centroid(d)
-        _d[0] *= 2.2 //multiply by a constant factor
-        _d[1] *= 2.2 //multiply by a constant factor
-        return 'translate(' + _d + ')'
-      })
-      .attr('dy', '.50em')
-      .style('text-anchor', 'middle')
-      .text(function(d) {
-        return d.data.percentage + '%'
-      })
+    // vis.svg
+    //   .selectAll('.arc')
+    //   .data(arcData)
+    //   .enter()
+    //   .append('text')
+    //   .attr('transform', function(d) {
+    //     var _d = arcGenerator.centroid(d)
+    //     _d[0] *= 1.7 //multiply by a constant factor
+    //     _d[1] *= 1.7 //multiply by a constant factor
+    //     return 'translate(' + _d + ')'
+    //   })
+    //   .attr('dy', '.50em')
+    //   .style('text-anchor', 'middle')
+    //   .text(function(d) {
+    //     return d.data.percentage + '%'
+    //   })
 
     //header
-    vis.svg
-      .append('text')
-      .attr('x', WIDTH / 2)
-      .attr('y', HEIGHT + 50)
-      .attr('text-anchor', 'middle')
-      .text('Sentiment Ratios')
+    // vis.svg
+    //   .append('text')
+    //   .attr('x', WIDTH / 2)
+    //   .attr('y', HEIGHT + 40)
+    //   .attr('text-anchor', 'middle')
+    //   .text('Sentiment Ratios')
   }
 }
