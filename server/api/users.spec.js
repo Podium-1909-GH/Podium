dfe23a623631c39b111a0232d3b1825f14cf7561
Mallory LeeWong <m.c.leewong@gmail.com>
2019-12-10 17:59:55 -0500

    feat: added 3 tests for 3 routes

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -1,13 +1,14 @@
 /* global describe beforeEach it */
 
 const {expect} = require('chai')
-const request = require('supertest')
+const supertest = require('supertest') // supertest object lets us make & test HTTP req/res
 const db = require('../db')
 const app = require('../index')
+const agent = supertest.agent(app)
 const User = db.model('user')
 
 describe('User routes', () => {
-  const codysEmail = 'cody@puppybook.com'
+  const codysEmail = 'cody@email.com'
   let user = {
     email: codysEmail,
     firstName: 'Cody',
@@ -18,20 +19,67 @@ describe('User routes', () => {
     return db.sync({force: true})
   })
 
-  describe('/api/users/', async () => {
-    beforeEach(() => {
-      return User.create(user)
+  describe('/api/users/', () => {
+    beforeEach(async () => {
+      await User.create(user)
     })
-    await request
-      .post('/auth/login')
-      .send({email: 'cody@email.com', password: '123'})
-    it('GET /api/users/:userId', async () => {
-      const res = await request(app)
-        .get('/api/users/1')
-        .expect(200)
 
-      expect(res.body).to.be.an('object')
-      expect(res.body.email).to.be.equal(codysEmail)
+    describe('POST /api/users/:userId/speeches', () => {
+      beforeEach(async () => {
+        await agent
+          .post('/auth/login')
+          .send({email: 'cody@email.com', password: '123'})
+      })
+
+      it('creates a new speech for a user by userId', async () => {
+        const res = await agent
+          .post(`/api/users/1/speeches`)
+          .expect(201)
+          .send({transcript: 'Hello this is me.', length: 4})
+
+        expect(res.body).to.be.an('object')
+        expect(res.body.transcript).to.be.equal('Hello this is me.')
+        expect(res.body.length).to.be.equal(4)
+        expect(res.body.userId).to.be.equal(1)
+      })
+    })
+
+    describe('GET /api/users/:userId/speeches', () => {
+      beforeEach(async () => {
+        await agent
+          .post('/auth/login')
+          .send({email: 'cody@email.com', password: '123'})
+      })
+
+      it('gets a speech for a user by userId', async () => {
+        const res = await agent
+          .get(`/api/users/1/speeches`)
+          .expect(200)
+          .send([{transcript: 'Hello'}])
+
+        expect(res.body).to.be.an('array')
+        // expect(res.body[0].transcript).to.be.equal('Hello')
+      })
+    })
+
+    describe('PUT /api/users/:userId', () => {
+      beforeEach(async () => {
+        await agent
+          .post('/auth/login')
+          .send({email: 'cody@email.com', password: '123'})
+      })
+
+      it('updates user first name and last name by ID', async () => {
+        const res = await agent
+          .put(`/api/users/1`)
+          .expect(200)
+          .send({firstName: 'CODYYY', lastName: 'PUGGG'})
+
+        expect(res.body).to.be.an('object')
+        expect(res.body.firstName).to.be.equal('CODYYY')
+        expect(res.body.lastName).to.be.equal('PUGGG')
+        expect(res.body.id).to.be.equal(1)
+      })
     })
   }) // end describe('/api/users')
 }) // end describe('User routes')
