ec4bb89bf69c731ec2642167ec587312a47f9086
Kiara Anderson <kiara.m.anderson@gmail.com>
2019-12-11 11:41:24 -0500

    added comments about databases

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -3,19 +3,8 @@ const Speech = require('./speech')
 const Word = require('./word')
 const WPM = require('./wpm')
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
-
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
+// make associations
+// export models so they can be accessed via db/models
 
 User.hasMany(Speech)
 Speech.belongsTo(User)
